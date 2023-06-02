81049c269a369661f36a11c33d7ace079c9990b5
RoelK <58561387+RoelKroes@users.noreply.github.com>
2023-06-02 15:40:38 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,19 +2,21 @@
 
 // Log decoded spots ready for upload to local file on disk
 const LOGSPOTS = true;
-const LOGFILE = "spotslog_pe2bz-10m.csv";
+const LOGFILE = "spotslog.csv";
 
 // Log raw wspr database queries to file on disk
 const LOGRAWSPOTS = true;    
-const RAWLOGFILE = "rawlog_pe2bz-10m.csv";
+const RAWLOGFILE = "rawlog.csv";
 
 // Upload spots to Sondehub
 const UPLOADSPOTS = true;
 
 // Balloon parameters
-const PAYLOAD = "BZ-HAB-13";
+// Name of the payload as show on Sondehub
+// CHANGE THIS
+const PAYLOAD = "MY_HAB";
 
-// use the correct number to search in a specific frequency band
+// use the correct number to search the wspr database for a specific frequency band
 // 3  = 80m
 // 7  = 40m
 // 14 = 20m
@@ -26,18 +28,20 @@ const PAYLOAD = "BZ-HAB-13";
 // 1296 = 23cm
 const FREQBAND = "28"; // Only get 10m spots
 
-// Uploader parameters (visible on sondehub)
+// Uploader parameters (visible on sondehub as the uploader)
 const UPLOADERCALL = "wspr2sondehub";
 
 // WSPR HAM CALL (your real HAM call, used by the WSPR transmissions from your HAB)
-const HAMCALL = "PE2BZ";
+// CHANGE THIS!
+const HAMCALL = "MYCALL";
 
 // Telemetry parameters
 // See: http://hojoham.blogspot.com/2016/10/known-flight-ids.html
-const CALLSIGN_SLOT = "2";
-const TELEMETRY_SLOT = "4";
+// CHANGE THIS
+const CALLSIGN_SLOT = "4";
+const TELEMETRY_SLOT = "6";
 const FLIGHT_ID_1 = "1";
-const FLIGHT_ID_3 = "8";
+const FLIGHT_ID_3 = "6";
 
 ?>
 
